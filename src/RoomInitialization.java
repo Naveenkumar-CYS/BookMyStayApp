668695853567e55b10637475accb1b2ba3e1ae84
Naveenkumar-CYS <naveenkumar2007cs@gmail.com>
2026-03-09 15:12:00 +0530

    UC3: Room Inventory using ArrayList

--- src/RoomInitialization.java
+++ src/RoomInitialization.java
@@ -1,83 +1,33 @@
-abstract class Room {
+import java.util.ArrayList;
 
-    protected String roomType;
-    protected int beds;
-    protected double size;
-    protected double price;
+class Room {
+    String roomType;
+    int price;
 
-    public Room(String roomType, int beds, double size, double price) {
+    Room(String roomType, int price) {
         this.roomType = roomType;
-        this.beds = beds;
-        this.size = size;
         this.price = price;
     }
 
-    public void displayRoomDetails() {
-        System.out.println("Room Type : " + roomType);
-        System.out.println("Beds      : " + beds);
-        System.out.println("Size      : " + size + " sq.ft");
-        System.out.println("Price     : ₹" + price + " per night");
+    void display() {
+        System.out.println("Room Type: " + roomType + " | Price: ₹" + price);
     }
 }
 
-
-/* Single Room */
-class SingleRoom extends Room {
-
-    public SingleRoom() {
-        super("Single Room", 1, 200, 2500);
-    }
-}
-
-
-/* Double Room */
-class DoubleRoom extends Room {
-
-    public DoubleRoom() {
-        super("Double Room", 2, 350, 4000);
-    }
-}
-
-
-/* Suite Room */
-class SuiteRoom extends Room {
-
-    public SuiteRoom() {
-        super("Suite Room", 3, 550, 7500);
-    }
-}
-
-
-/* Main Class for Use Case 2 */
-public class UseCase2RoomInitialization {
+public class RoomInitialization {
 
     public static void main(String[] args) {
 
-        System.out.println("====================================");
-        System.out.println("        BOOK MY STAY APP");
-        System.out.println("     Hotel Booking System v2.1");
-        System.out.println("====================================");
+        System.out.println("BOOK MY STAY APP - Room Inventory v3.0\n");
 
-        Room single = new SingleRoom();
-        Room doubleRoom = new DoubleRoom();
-        Room suite = new SuiteRoom();
+        ArrayList<Room> rooms = new ArrayList<>();
 
-        int singleAvailability = 5;
-        int doubleAvailability = 3;
-        int suiteAvailability = 2;
+        rooms.add(new Room("Single Room", 2500));
+        rooms.add(new Room("Double Room", 4000));
+        rooms.add(new Room("Suite Room", 7500));
 
-        System.out.println("\nAvailable Room Types\n");
-
-        single.displayRoomDetails();
-        System.out.println("Available Rooms: " + singleAvailability);
-        System.out.println("------------------------------------");
-
-        doubleRoom.displayRoomDetails();
-        System.out.println("Available Rooms: " + doubleAvailability);
-        System.out.println("------------------------------------");
-
-        suite.displayRoomDetails();
-        System.out.println("Available Rooms: " + suiteAvailability);
-        System.out.println("------------------------------------");
+        for (Room r : rooms) {
+            r.display();
+        }
     }
 }
